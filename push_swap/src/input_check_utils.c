7e56032bbec6ab5201b70c246cc1502acfedc5e3
Talibabtou <talibabtou@gmail.com>
2024-01-15 16:29:04 +0100

    Update input_check_utils.c

--- push_swap/src/input_check_utils.c
+++ push_swap/src/input_check_utils.c
@@ -6,7 +6,7 @@
 /*   By: gdumas <gdumas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:22:44 by gdumas            #+#    #+#             */
-/*   Updated: 2024/01/08 17:55:49 by gdumas           ###   ########.fr       */
+/*   Updated: 2024/01/15 12:31:36 by gdumas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,33 @@ int	nbstr_cmp(const char *s1, const char *s2)
 		j++;
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[j]);
+}
+
+void	ft_strtok(char **av)
+{
+	static char	*buffer;
+	char		*token;
+	int			i;
+
+	buffer = av[1];
+	if (av[1] && (*buffer != '\0'))
+	{
+		token = buffer;
+		i = 1;
+		while (*buffer)
+		{
+			if (*buffer == ' ')
+			{
+				*buffer = '\0';
+				av[i++] = token;
+				token = buffer + 1;
+			}
+			buffer++;
+		}
+		if (buffer != token || *buffer != '\0')
+			av[i++] = token;
+		av[i] = NULL;
+	}
+	else
+		exit(0);
 }
